50e8cf689c820b56d3e19dcba9bb80e6cd2c7953
itachi-uchiha12 <163325793+itachi-uchiha12@users.noreply.github.com>
2024-06-26 10:18:37 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class count{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n=sc.nextInt();
+        String a= Integer.toString(n);
+        System.out.println("The length of the number is: "+ a.length());
+    }
+}
+
+
+ /*output
+
+Enter the number:23456
+The length of the number is: 5 */
